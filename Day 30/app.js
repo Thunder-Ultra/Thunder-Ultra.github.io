7fcbb168cedfc066351b115ad1763ad703bb9f36
Tuchar <dastuchar@gmail.com>
2024-12-30 10:32:57 +0530

    Added Day 30

---
+++ Day 30/app.js
@@ -0,0 +1,8 @@
+let age=32;
+let username='Tuchar';
+let hobbies=['Calisthenics','Cooking','Coding'];
+let job = { title:'Developer', place:'New Delhi', salary:50000 };
+
+alert(hobbies[1]);
+alert(job.title);
+
