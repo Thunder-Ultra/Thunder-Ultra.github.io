21adc7e414cbea2cfe09a52a4ca3c670296b70d1
Tuchar <dastuchar@gmail.com>
2025-04-08 14:36:51 +0530

    Added Day 44 and completed the program myself

--- Day 42/Day 41/scripts/config.js
+++
@@ -1,9 +0,0 @@
-function openPlayerConfig() {
-    backdropElement.style.display="block";
-    playerConfigOverlayElement.style.display="block";
-}``
-
-function closePlayerConfig() {
-    backdropElement.style.display="none";
-    playerConfigOverlayElement.style.display="none";
-}
